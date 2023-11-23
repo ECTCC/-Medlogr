6d3b65b1e636d4b9e00d0fb24a1710de206b5e2a
Ziriado <115992429+Ziriado@users.noreply.github.com>
2023-11-23 16:05:07 +0100

    connection string is changed
    For log in.

--- µMedlogr/Data/Migrations/20231123145641_Init.Designer.cs
+++ µMedlogr/Data/Migrations/20231123145641_Init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using µMedlogr.Data;
 
@@ -11,9 +12,11 @@ using µMedlogr.Data;
 namespace µMedlogr.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231123145641_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
