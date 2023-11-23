c7f63f5a83b94b84d5f401cc874bddfa6f5d577b
Bertwald <thomas_ace8@hotmail.com>
2023-11-23 17:12:11 +0100

    Enable migration for core library

--- µMedlogr.core/Models/HealthRecord.cs
+++ µMedlogr.core/Models/HealthRecord.cs
@@ -1,6 +1,9 @@
-﻿using µMedlogr.core.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using µMedlogr.core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace µMedlogr.core.Models;
+[PrimaryKey(nameof(Id))]
 internal class HealthRecord {
     internal int Id { get; set; }
     internal required Person Record { get; set; }
---
+++ µMedlogr.core/µMedlogrContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace µMedlogr.core;
+internal class µMedlogrContextFactory : IDesignTimeDbContextFactory<µMedlogrContext> {
+    public µMedlogrContext CreateDbContext(string[] args) {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine( Directory.GetCurrentDirectory() ,  "../µMedlogr" ))
+            .AddJsonFile("appsettings.json").Build();
+
+        var connstring = config.GetConnectionString("DefaultConnection");
+        var optionsBuilder = new DbContextOptionsBuilder<µMedlogrContext>();
+        optionsBuilder.UseSqlServer(connstring);
+
+        return new µMedlogrContext(optionsBuilder.Options);
+    }
+}
