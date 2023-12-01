e43c4368c31dac1b3687d3a8fdaf2c7184b8d68a
Ziriado <115992429+Ziriado@users.noreply.github.com>
2023-12-01 10:45:21 +0100

    Added that you can only add people if logged in.

--- µMedlogr/Pages/PersonPage.cshtml.cs
+++ µMedlogr/Pages/PersonPage.cshtml.cs
@@ -8,10 +8,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace µMedlogr.Pages
 {
-    public class PersonPageModel(EntityManager entityManager,UserManager<AppUser> userManager) : PageModel
+    public class PersonPageModel(EntityManager entityManager, UserManager<AppUser> userManager) : PageModel
     {
         private readonly EntityManager _entityManager = entityManager;
-        private readonly UserManager<AppUser> _userManager =userManager;
+        private readonly UserManager<AppUser> _userManager = userManager;
 
         [BindProperty]
         public Person Person { get; set; }
@@ -22,19 +22,24 @@ namespace µMedlogr.Pages
         [BindProperty]
         public DateOnly SelectedDate { get; set; }
         public AppUser MyUser { get; set; }
-        public async void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
             AllergiesList = core.Services.PersonPage.CreateAllergiesList();
+            MyUser = await _userManager.GetUserAsync(User);
+            return Page();
         }
         public async Task<IActionResult> OnPostSavePersonAsync()
         {
-            MyUser = await _userManager.GetUserAsync(User);MyUser = await _userManager.GetUserAsync(User);
+            MyUser = await _userManager.GetUserAsync(User);
             Person.Allergies = core.Services.PersonPage.ReturnSameListOrAddStringNoAllergy(SelectedAllergies);
-            Person.DateOfBirth=SelectedDate;
-            var healthrecord=new core.Models.HealthRecord();
+            Person.DateOfBirth = SelectedDate;
+            var healthrecord = new core.Models.HealthRecord();
             healthrecord.Person = Person;
-            Person.HealthRecord=healthrecord;
-            MyUser.PeopleInCareOf.Add(Person);
+            Person.HealthRecord = healthrecord;
+            if (MyUser is not null)
+            {
+                MyUser.PeopleInCareOf.Add(Person);
+            }
             await _entityManager.SaveNewPerson(Person);
             return RedirectToPage("/PersonPage");
         }
