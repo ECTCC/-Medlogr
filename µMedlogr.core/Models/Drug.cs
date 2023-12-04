20c0bfda1abae61423eb7f8c874fbcb05483c5c9
Bertwald <thomas_ace8@hotmail.com>
2023-12-04 13:53:24 +0100

    Add Enums for Medicine Events

--- µMedlogr.core/Models/Drug.cs
+++ µMedlogr.core/Models/Drug.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using µMedlogr.core.Enums;
 
 namespace µMedlogr.core.Models;
 [PrimaryKey(nameof(Id))]
