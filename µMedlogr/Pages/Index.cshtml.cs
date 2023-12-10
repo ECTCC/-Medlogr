6a011453242e5b37bcd6c06bfeac95e6ca199ffc
Bertwald <thomas_ace8@hotmail.com>
2023-12-10 10:57:04 +0100

    Remove unused members and properties, Remove unnecessary injection

--- µMedlogr/Pages/Index.cshtml.cs
+++ µMedlogr/Pages/Index.cshtml.cs
@@ -7,15 +7,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace µMedlogr.Pages;
 
-public class IndexModel(UserManager<AppUser> userManager, EntityManager entityManager, PersonService personService, µMedlogrContext context) : PageModel
+public class IndexModel(UserManager<AppUser> userManager, PersonService personService) : PageModel
 {
     private readonly UserManager<AppUser> _userManager = userManager;
     private readonly PersonService _personService = personService;
-    //private readonly EntityManager _entityManager = entityManager;
-    //private readonly µMedlogrContext _context = context;
+
     public AppUser? MyUser { get; set; }
     public Person? Me { get; set; }
-    //public int HealthRecordId { get; set; }
+
     public List<Person> PeopleInCareOf { get; set; }
 
     public async Task<IActionResult> OnGetAsync()
